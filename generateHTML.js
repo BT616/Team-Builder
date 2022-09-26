07dc9635480a90d124e8bc8513c7f2561367928e
BT616 <110855674+BT616@users.noreply.github.com>
2022-09-26 16:42:40 -0700

    making the html function for the employees

---
+++ generateHTML.js
@@ -0,0 +1,82 @@
+
+const generateManager = function (manager){
+    return`
+    <div class="container-fluid mt-3">
+            <div class="row justify-content-center">
+
+                <!--manager-->
+                <div class="card" style="width: 18rem;">
+
+                    <div class="card-body">
+                        <h5 class="card-title">${manager.name}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
+                        <p class="card-text">${manager.id}</p>
+                        <p class="card-text">${manager.email}</p>
+                        <p class="card-text">${manager.officeNumber}</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+
+    `
+};
+
+const generateEmployee = function(employee){
+
+    return`
+    <!--employee-->
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${employee.name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
+            <p class="card-text">${employee.id}</p>
+            <p class="card-text">${employee.email}</p>
+            <p class="card-text">${office-number}</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+        </div>
+    </div>
+    `
+}
+
+const generateEngineer = function(engineer){
+   return`
+    <!--engineer-->
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${engineer.name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Engineer</h6>
+            <p class="card-text">${enineer.id}</p>
+            <p class="card-text">${engineer.email}</p>
+            <p class="card-text">${engineer.github}</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+        </div>
+    </div>
+
+
+</div>
+</div>
+`
+}
+const generateIntern = function(intern){
+    return`
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">${intern.name}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
+        <p class="card-text">${intern.id}</p>
+        <p class="card-text">${intern.email}</p>
+        <p class="card-text">${intern.school}</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+    </div>
+</div>
+</div>`
+}
+
+}
+
+
+
+module.exports = generateHTML;
--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 
-const inquirer =require('inquirer');
-const fs=require('fs');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
+const generateHTML = require(answers);
 
-
-
-
-const generateHTML = ({})
-
-`
+    `
 <!DOCTYPE html>
 <html lang="en">
 
@@ -28,8 +24,11 @@ const generateHTML = ({})
         </div>
     </header>
 
+`
 
 
+generate manager= getRole(manager){
+    return `
     <main>
 
         <div class="container-fluid mt-3">
@@ -39,8 +38,8 @@ const generateHTML = ({})
                 <div class="card" style="width: 18rem;">
 
                     <div class="card-body">
-                        <h5 class="card-title">${managername}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
+                        <h5 class="card-title"></h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${manager}</h6>
                         <p class="card-text">${id}</p>
                         <p class="card-text">${email}</p>
                         <p class="card-text">${office-number}</p>
@@ -49,22 +48,24 @@ const generateHTML = ({})
                     </div>
                 </div>
 
-
-
+                `
+};
+generate employee = getRole(manager){
+`
                 <!--employee-->
                 <div class="card" style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">${employeename}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
+                        <h6 class="card-subtitle mb-2 text-muted"></h6>
                         <p class="card-text">${id}</p>
                         <p class="card-text">${email}</p>
-                        <p class="card-text">${office-number}</p>
+                        <p class="card-text">${office - number}</p>
                         <a href="#" class="card-link">Card link</a>
                         <a href="#" class="card-link">Another link</a>
                     </div>
                 </div>
-
-
+`
+};
 
                 <!--engineer-->
                 <div class="card" style="width: 18rem;">
@@ -126,4 +127,179 @@ const generateHTML = ({})
 </main>
 
 </html>
+<script src= " https://code.jquery.com/jquery-3.6.1.min.js"</script>
 `
+(async function (){ 
+const questions = await [{
+    
+
+    
+    type: "input",
+    name: "manager",
+    message: "whats the name of your manager?",
+},
+{
+    type: "input",
+    name: "email",
+    message: "what is your managers email?"
+},
+{
+    type: "input",
+    name: "manager-id",
+    message: "what is your managers ID?"
+},
+{
+    type: "input",
+    name: "office",
+    message: "what is your managers office number?"
+},
+{
+    type: "list",
+    name: "menu",
+    message: "Do you have other employee info you want to enter??",
+    choices: ["yes/no"]
+},
+{
+    type: "list",
+    name: "menu",
+    message: "who?",
+    choices: ["employee,engineer,intern"],
+    when(answers) {
+        return answers.menu === "yes"
+    }
+},
+{
+    type: "input",
+    name: "engineer",
+    message: "whats the name of your other engineer?",
+    when(answers) {
+        return answers.menu === "engineer"
+    }
+},
+{
+    type: "input",
+    name: "engineer-github",
+    message: "whats thier github username?",
+    when(answers) {
+        return answers.menu === "engineer"
+    }
+},
+{
+    type: "input",
+    name: "engineer-email",
+    message: "whats thier email??",
+    when(answers) {
+        return answers.menu === "engineer"
+    }
+},
+{
+    type: "input",
+    name: "engineer-id",
+    message: "whats thier ID number?",
+    when(answers) {
+        return answers.menu === "engineer"
+    }
+},
+
+// this this will run after, if you selected engineer
+{
+    type: "list",
+    name: "menu",
+    message: "who?",
+    choices: ["employee,intern"],
+    
+
+},
+// not sure anymore but employee
+{
+    type: "input",
+    name: "employee",
+    message: "whats the name of your employee?",
+    when(answers) {
+        return answers.menu === "employee"
+    }
+},
+{
+    type: "input",
+    name: "employee-github",
+    message: "whats thier github username?",
+    when(answers) {
+        return answers.menu === "employee"
+    }
+},
+{
+    type: "input",
+    name: "employee-email",
+    message: "whats thier email??",
+    when(answers) {
+        return answers.menu === "employee"
+    }
+},
+{
+    type: "input",
+    name: "employee-id",
+    message: "whats thier ID number?",
+    when(answers) {
+        return answers.menu === "employee"
+    }
+},
+{
+    type: "list",
+    name: "menu",
+    message: "who?",
+    choices: ["engineer,intern"],
+    
+
+},
+
+//intern 
+{
+    type: "input",
+    name: "intern",
+    message: "whats the name of your intern?",
+    when(answers) {
+        return answers.menu === "intern"
+    }
+},
+{
+    type: "input",
+    name: "intern-email",
+    message: "whats thier email??",
+    when(answers) {
+        return answers.menu === "intern"
+    }
+},
+{
+    type: "input",
+    name: "intern-id",
+    message: "whats thier ID number?",
+    when(answers) {
+        return answers.menu === "intern"
+    }
+},
+{
+    type: "input",
+    name: "intern-school",
+    message: "where did they go to school?",
+    when(answers) {
+        return answers.menu === "intern"
+    }
+},
+{
+    type: "input",
+    name: "employee-2",
+    message: "where did they go to school?",
+    when(answers) {
+        return answers.menu === "intern"
+    }
+},
+{
+    type: "list",
+    name: "menu",
+    message: "who?",
+    choices: ["employee,engineer"],
+    
+
+},
+]
+
